5f7e1abdabc1df3d245d54911cdf6c6c1f7a537b
Tunahan Akpınar <tunahan.81.jb@gmail.com>
2025-08-13 14:27:50 +0300

    fix: Next.js deploy sorununu geçici olarak çözmek için image optimization'ı kapat

--- next.config.ts
+++ next.config.ts
@@ -2,5 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["http://localhost:3000", "http://172.20.41.70:3000"],
+  images: {
+    // Geçici olarak image optimization’ı kapatıyoruz
+    unoptimized: true,
+  },
 };
+
 export default nextConfig;
