cacdf509f3be8ae8219a238d254fa4ebbd31cf82
Tunahan Akpınar <tunahan.81.jb@gmail.com>
2025-08-13 11:43:33 +0300

    test: CI/CD pipeline check

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["http://localhost:3000", "http://172.20.41.70:3000"],
 };
-
 export default nextConfig;
